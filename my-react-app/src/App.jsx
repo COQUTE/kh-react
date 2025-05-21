4d20cca67f4f4f32a8cd2ac1aa9c2a9f5673ac04
COQUTE <kimjiho246@naver.com>
2025-05-21 10:54:32 +0900

    React State (liftingUp) & Props (drilling) & JSX 예제

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {useState} from "react";
 import Exam2 from "./components/Exam2.jsx";
+import Exam3 from "./components/Exam3.jsx";
+import Exam4 from "./components/Exam4.jsx";
+import Exam5 from "./components/Exam5.jsx";
 
 function App() {
   // 상태(state)
@@ -17,12 +20,17 @@ function App() {
       <h1>Hello World</h1>
     </>
      */
-    <>
-      <button onClick={() => setShowExam(!showExam)}>클릭</button>
 
-      {/* showExam이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함 */}
-      {showExam && <Exam2 jiho={"hello"} test={"world"} />}
-    </>
+    // <>
+    //   <button onClick={() => setShowExam(!showExam)}>클릭</button>
+    //
+    //   {/* showExam이 true면 화면에 Exam1 컴포넌트 호출하여 렌더링함 */}
+    //   {showExam && <Exam2 jiho={"hello"} test={"world"} />}
+    // </>
+
+    // <Exam3/>
+    // <Exam4/>
+    <Exam5/>
   );
 }
 
